417882bdb694611e65572599da9aef80b5d485ce
xiaocaiji233 <1579957671@qq.com>
2019-12-23 08:08:09 +0800

    update

--- handler/hello.py
+++ handler/hello.py
@@ -1,17 +1,22 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 from app import route, response, redirect, config
 
 import logic.Team1901.reposInfo as a
-repos = ['cxsjclassroom/webserver',"octocat/Hello-World"]
+import json
+
+repos = ['cxsjclassroom/webserver', 'octocat/Hello-World', 'apache/hive', 'apache/cassandra', 'apache/camel', 'apache/commons-lang']
+
 
 @route('/hello.py.html')
 def projectInfo():
     info = a.getRepoInfo()
-#将info返回给页面
-    return response(projectInfo=info)
+    info_json = json.dumps(info)
+    # 将info返回给页面
+    return response(projectInfo=info, projectJson=info_json)
 
-#显示部分信息的新界面
+
+# 显示部分信息的新界面
 @route('/diy.py.html')
 def diyInfo():
     info = a.getRepoInfo()
@@ -21,5 +26,5 @@ def diyInfo():
 @route('/commits.py.html')
 def commits():
     info = a.getRepoInfo()
-#将info返回给页面
+    # 将info返回给页面
     return response(projectInfo=info)
--- server/logic/Team1901/reposInfo.py
+++ server/logic/Team1901/reposInfo.py
@@ -5,47 +5,55 @@ import os
 import json
 import pprint
 
-repos = ['cxsjclassroom/webserver', "octocat/Hello-World", "apache/logging-log4j2"]
+repos = ['cxsjclassroom/webserver', 'octocat/Hello-World', 'apache/hive', 'apache/cassandra', 'apache/camel', 'apache/commons-lang']
 
 
-def getCommitsCount(repo):
-    page = 1
-    count = 0
+def getContributorsCount(repo):
+    url = 'https://api.github.com/repos/' + repo + '/stats/contributors'
+    contributorsInfo = readURL('Repositories/contributors_count/%s' % repo, url)
+    contributorsInfo = contributorsInfo and json.loads(contributorsInfo)
 
-    url = 'https://api.github.com/repos/' + repo + '/commits?page=' + str(page)
-    commitsInfo = readURL('Repositories/commitsInfo/%s_page_%s' % (repo, str(page)), url)
-    commitsInfo = commitsInfo and json.loads(commitsInfo)
-    count += len(commitsInfo)
-    while True:
-        if len(commitsInfo) == 0:
-            break
+    for contributor in contributorsInfo:
+        print("Contributor: %s, count:  %s \n" % (contributor['author']['login'], contributor['total']))
 
-        page += 1
-        url = 'https://api.github.com/repos/' + repo + '/commits?page=' + str(page)
-        commitsInfo = readURL('Repositories/commitsInfo/%s_page_%s' % (repo, str(page)), url)
-        commitsInfo = commitsInfo and json.loads(commitsInfo)
-        count += len(commitsInfo)
-        pprint.pprint(commitsInfo)
-        for c in commitsInfo:
-            input(c)
-        print(repo + ': ' + str(len(commitsInfo)) + ' Page: ' + str(page) + '\n')
-    return count
+
+def getCommitsCountByCmd(name):
+    gitClone(name)
+    projectPath = os.path.abspath('data/gitRepo/%s' % name)  # 保存的路径
+    cmd = 'git rev-list --all --count>commit.txt'
+    cwd = os.getcwd()  # 记录当前目录
+    os.chdir(projectPath)  # 跳转到需要保存代码库的路径
+    if gitClone(name) is True:
+        result = os.system(cmd)  # 执行shell命令
+        os.chdir(cwd)  # 跳转回到当前目录
+        return str(result)
+
+    os.chdir(cwd)  # 跳转回到当前目录
+    return False
 
 
 def getRepoInfo():
     info = {}
     for repo in repos:
         repo_url = 'https://api.github.com/repos/%s' % repo  # 确定url
-        repoInfo = readURL('Repositories/reposInfo/%s' % (repo), repo_url)  # 访问url得到数据
+        repoInfo = readURL('Repositories/reposInfo/%s' % repo, repo_url)  # 访问url得到数据
         repoInfo = repoInfo and json.loads(repoInfo)  # 将数据类型转换
-        #pprint.pprint(repoInfo)
-        branchInfo = readURL('Repositories/branchInfo/%s' % (repo), repoInfo['branches_url'].replace('{/branch}', ''))
+        branchInfo = readURL('Repositories/branchInfo/%s' % repo, repoInfo['branches_url'].replace('{/branch}', ''))
         branchInfo = branchInfo and json.loads(branchInfo)
-        # pprint.pprint(branchInfo)
-        commitsInfo = readURL('Repositories/commitsInfo/%s' % (repo), repoInfo['commits_url'].replace('{/sha}', ''))
+        commitsInfo = readURL('Repositories/commitsInfo/%s' % repo, repoInfo['commits_url'].replace('{/sha}', ''))
         commitsInfo = commitsInfo and json.loads(commitsInfo)
-        # pprint.pprint(commitsInfo)
-        gitClone('cxsjclassroom/webserver')
+        contributorsInfo = readURL('Repositories/commitsInfo/%s' % repo, repoInfo['contributors_url'])
+        contributorsInfo = contributorsInfo and json.loads(contributorsInfo)
+        # releasesInfo = readURL('Repositories/releasesInfo/%s' % repo, repoInfo['releases_url'].replace('{/id}', ''))
+        # releasesInfo = releasesInfo and json.loads(releasesInfo)
+        JMHurl = 'https://api.github.com/search/code?q=org.openjdk.jmh+in:file+language:java+repo:%s' % repo
+        jmhInfo = readURL('Repositories/jmhInfo/%s' % repo, JMHurl)
+        jmhInfo = jmhInfo and json.loads(jmhInfo)
+        # getContributorsCount(repo)
+        getCommitsCountByCmd(repo)
+        f = open('data/gitRepo/%s/commit.txt' % repo)
+        commits_counts = f.read()
+        f.close()
 
         # 提取想要的信息保存在info中
         info[repo] = {
@@ -59,20 +67,24 @@ def getRepoInfo():
             'branches_count': len(branchInfo),
             'branches_name': list(map(lambda b: b['name'], branchInfo)),
             'commits': commitsInfo,
-            'commits_count': getCommitsCount(repo),
-
+            'contributors_count': len(contributorsInfo),
+            # 'releases_count': len(releasesInfo),
+            'commits_count': commits_counts,
+            'JMH_count': jmhInfo['total_count']
         }
-
+    gitClone(repo)
+    gitLog(repo)
+    gitJmh(repo)
     return info
 
 
 def gitClone(name):
-    projectPath = os.path.abspath('data/gitRepo/%s' % (name))  # 保存的路径
+    projectPath = os.path.abspath('data/gitRepo/%s' % name)  # 保存的路径
     not os.path.isdir(projectPath) and os.makedirs(projectPath)
     if os.path.exists(projectPath):
         return True
 
-    cmd = 'git clone %s.ghttp://github.com/it %s' % (name, projectPath)
+    cmd = 'git clone http://github.com/%s.git %s' % (name, projectPath)
     cwd = os.getcwd()  # 记录当前目录
     os.chdir(projectPath)  # 跳转到需要保存代码库的路径
     result = os.system(cmd)  # 执行shell命令
@@ -82,13 +94,48 @@ def gitClone(name):
     return True
 
 
+def gitJmh(name):
+    JMHurl = 'https://api.github.com/search/code?q=org.openjdk.jmh+in:file+language:java+repo:%s' % name
+    jmhInfo = readURL('Repositories/jmhInfo/%s' % name, JMHurl)
+    jmhInfo = jmhInfo and json.loads(jmhInfo)
+    projectPath = os.path.abspath('data/gitRepo/%s' % name)
+    cwd = os.getcwd()  # 记录当前目录
+    os.chdir(projectPath)  # 跳转到需要保存代码库的路径
+    file1 = r'jmh1.txt'
+    file2 = r'jmh2.txt'
+    if os.path.exists(file1) and os.path.exists(file2):
+        os.remove(file1)
+        os.remove(file2)
+    for i, item in enumerate(jmhInfo['items']):
+        cmd = 'git shortlog -s -n head %s>temp.txt ' % item['path']
+        result = os.system(cmd)  # 执行shell命令
+        with open('temp.txt', 'r')as fa:
+            contest = fa.read()
+            with open(file1, 'a+')as f:
+                f.write(contest + '\n')
+
+    os.remove('temp.txt')
+    for i, item in enumerate(jmhInfo['items']):
+        cmd = 'git log --pretty=format:"%h|%ce|%cd" ' + str(item['path']) + ' > temp.txt'
+        result = os.system(cmd)  # 执行shell命令
+        with open('temp.txt', 'r')as fb:
+            contest = fb.read()
+            with open(file2, 'a+')as f:
+                f.write(contest + '\n')
+
+    os.remove('temp.txt')
+    os.chdir(cwd)  # 跳转回到当前目录
+    return result
+
+
 def gitLog(name):
-    projectPath = os.path.abspath('data/gitRepo/%s' % (name))  # 保存的路径
+    projectPath = os.path.abspath('data/gitRepo/%s' % name)  # 保存的路径
     cmd = 'git log --pretty=format:"%h -%an,%ar: %s">log.txt'
     cwd = os.getcwd()  # 记录当前目录
     os.chdir(projectPath)  # 跳转到需要保存代码库的路径
     if gitClone(name) is True:
         result = os.system(cmd)  # 执行shell命令
+        os.chdir(cwd)  # 跳转回到当前目录
         return result
 
     os.chdir(cwd)  # 跳转回到当前目录
